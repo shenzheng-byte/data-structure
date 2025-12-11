e7e970a21976757e6bf090291b2ae142625f9500
red star <3391807502@qq.com>
2025-12-11 11:29:04 +0800

    modified a little

--- arrayList.cpp
+++ arrayList.cpp
@@ -5,7 +5,14 @@ int main(){
         a.push_back(i);
     }
     for(int i=0;i<20;i++){
-        cout<<a[i]<<endl;
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    arrayList<int>::iterator it=a.begin();
+    arrayList<int>::iterator end=a.end();
+    while(it!=end){
+        cout<<*it<<endl;
+        it++;
     }
     return 0;
 }
